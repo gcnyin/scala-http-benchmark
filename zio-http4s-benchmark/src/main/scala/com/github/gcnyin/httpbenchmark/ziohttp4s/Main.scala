09781fdb471a6d8925b7a634c155de9ff448e438
rohanverma94 <rohan40594@gmail.com>
2022-09-18 06:06:47 +0530

    Added Zio2 based http4s server

---
+++ zio-http4s-benchmark/src/main/scala/com/github/gcnyin/httpbenchmark/ziohttp4s/Main.scala
@@ -0,0 +1,35 @@
+package com.github.gcnyin.httpbenchmark.ziohttp4s
+
+import org.http4s.HttpRoutes
+import org.http4s.dsl.Http4sDsl
+import org.http4s.ember.server.EmberServerBuilder
+import org.http4s.server.Server
+import com.comcast.ip4s._
+import zio.{ExitCode, Scope, Task, URIO, ZIO, ZIOAppArgs, ZIOAppDefault, ZLayer}
+import zio.interop.catz._
+
+object Main extends ZIOAppDefault {
+
+  object getRoutes extends Http4sDsl[Task] {
+    val zio_routes: HttpRoutes[Task] = HttpRoutes.of {
+      case GET -> Root => Ok("Hello, world!")
+    }
+  }
+
+  val zioHttp4sLayer: ZLayer[Any, Throwable, Server] =
+    ZLayer.scoped {
+      EmberServerBuilder.default[Task]
+        .withHost(host = ipv4"0.0.0.0")
+        .withPort(port = port"8080")
+        .withHttpApp(getRoutes.zio_routes.orNotFound)
+        .build
+        .toScopedZIO
+    }
+
+
+  val entry_point: URIO[Any, ExitCode] = ZIO.never.exitCode
+
+  def run: ZIO[Any with ZIOAppArgs with Scope, Throwable, ExitCode] = entry_point.provideLayer(
+    zioHttp4sLayer).catchAll(t => ZIO.succeed(t.printStackTrace()).map(_ => ExitCode.failure))
+
+}
