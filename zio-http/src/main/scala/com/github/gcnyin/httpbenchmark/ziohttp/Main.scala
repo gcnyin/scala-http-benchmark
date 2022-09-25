19c83545b49818880127a03714d8cc0999f3931b
gcnyin <guchounongyin@gmail.com>
2022-09-25 16:20:55 +0800

    revert zio-http implementation

--- zio-http/src/main/scala/com/github/gcnyin/httpbenchmark/ziohttp/Main.scala
+++ zio-http/src/main/scala/com/github/gcnyin/httpbenchmark/ziohttp/Main.scala
@@ -5,10 +5,13 @@ import zhttp.service.Server
 import zio._
 
 object Main extends ZIOAppDefault {
-  def app(frozen: Response): Http[Any, Nothing, Request, Response] = Http.collect[Request] { case Method.GET -> !! =>
-    frozen
+  val app: Http[Any, Nothing, Request, Response] = {
+    Http.collect[Request] { case Method.GET -> !! =>
+      Response.text("Hello, world!")
+    }
   }
 
-  override def run: ZIO[Any, Any, Any] =
-    Response.text("Hello, world!").freeze.flatMap(frozen => Server.start(8080, app(frozen))).exitCode
+  override def run: ZIO[Any, Any, Any] = {
+    Server.start(8080, app).exitCode
+  }
 }
