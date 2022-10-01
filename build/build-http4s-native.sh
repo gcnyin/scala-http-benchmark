579357588672f4081bc0970d5c6fc94a04830981
gcnyin <guchounongyin@gmail.com>
2022-10-01 23:43:40 +0800

    fix

--- build/build-http4s-native.sh
+++ build/build-http4s-native.sh
@@ -2,6 +2,8 @@
 
 cd $(dirname $0)
 
+cd ..
+
 echo $(pwd)
 
-docker build . -t http4s-ce3-native-benchmark:0.1.0 -f ./http4s-native/Dockerfile
+docker build . -t http4s-native-benchmark:0.1.0 -f build/http4s-native/Dockerfile
