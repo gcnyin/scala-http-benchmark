b77e920808a8cd96474e6c71d1c1346034febfb6
gcnyin <guchounongyin@gmail.com>
2022-12-11 14:38:43 +0800

    [vertx-web] updated logging

--- vertx-web/src/main/scala/com/github/gcnyin/httpbenchmark/vertxweb/Main.scala
+++ vertx-web/src/main/scala/com/github/gcnyin/httpbenchmark/vertxweb/Main.scala
@@ -3,12 +3,15 @@ package com.github.gcnyin.httpbenchmark.vertxweb
 import io.vertx.core.{AbstractVerticle, Promise, Vertx}
 import io.vertx.core.http.HttpMethod
 import io.vertx.ext.web.Router
+import org.slf4j.{Logger, LoggerFactory}
 
 object Main {
+  private val logger: Logger = LoggerFactory.getLogger(classOf[Main.type])
+
   def main(args: Array[String]): Unit = {
     val vertx = Vertx.vertx()
-    val processors = Runtime.getRuntime().availableProcessors()
-    for (i <- 1 to processors) {
+    val processors = Runtime.getRuntime.availableProcessors()
+    for (_ <- 1 to processors) {
       vertx.deployVerticle(new HttpServerVerticle())
     }
   }
@@ -19,10 +22,11 @@ object Main {
       val router = Router.router(vertx)
       router.route(HttpMethod.GET, "/").handler { ctx =>
         val response = ctx.response
-        response.putHeader("Content-Type", "text/plain")
+        response.putHeader("Content-Type", "text/plain; charset=utf-8")
         response.end("Hello, world!")
       }
       server.requestHandler(router).listen(8080).andThen { _ =>
+        logger.info("verticle deployed: {}", deploymentID())
         startPromise.complete()
       }
     }
